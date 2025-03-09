564531b0d5918dcdd7f97121c12937a53c51e663
Jordi <jordiws@gmail.com>
2025-03-09 17:54:10 -0300

    Select by response time is working

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/santosjordi/posgoexp/challenge2
+
+go 1.23.2
--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 type ViaCEP struct {
@@ -25,27 +25,90 @@ type BrasilApiCep struct {
 	Bairro     string `json:"neighborhood"`
 }
 
+// Isso é uma gambiarra?
+type CEP struct {
+	Api        string
+	Cep        string `json:"cep"`
+	Logradouro string `json:"logradouro"`
+	Estado     string `json:"estado"`
+	Cidade     string `json:"cidade"`
+	Bairro     string `json:"bairro"`
+}
+
+func (c CEP) PrintAsMap() {
+	properties := map[string]string{
+		"Api":        c.Api,
+		"Cep":        c.Cep,
+		"Logradouro": c.Logradouro,
+		"Estado":     c.Estado,
+		"Cidade":     c.Cidade,
+		"Bairro":     c.Bairro,
+	}
+	for key, value := range properties {
+		log.Printf("%s: %s\n", key, value)
+	}
+}
+
+func convertViaCEP(v ViaCEP) CEP {
+	return CEP{
+		Api:        "ViaCEP",
+		Cep:        v.Cep,
+		Logradouro: v.Logradouro,
+		Estado:     v.Estado,
+		Cidade:     v.Cidade,
+		Bairro:     v.Bairro,
+	}
+}
+
+func convertBrasilApiCep(b BrasilApiCep) CEP {
+	return CEP{
+		Api:        "BrasilAPI",
+		Cep:        b.Cep,
+		Logradouro: b.Logradouro,
+		Estado:     b.Estado,
+		Cidade:     b.Cidade,
+		Bairro:     b.Bairro,
+	}
+}
+
+// [X] Query the same CEP from two different APIs
+// [X] Acatar a API que entregar a resposta mais rápida e descartar a resposta mais lenta.
+// [X] O resultado da request deverá ser exibido no command line com os dados do endereço, bem como qual API a enviou.
+// [X] Limitar o tempo de resposta em 1 segundo. Caso contrário, o erro de timeout deve ser exibido.
 func main() {
 	for _, cep := range os.Args[1:] {
-		response, err := BuscaViaCep(cep)
-		if err != nil {
-			log.Fatalf("Error getting cep: %v", err)
-		}
-		fmt.Println("ViaCEP")
-		fmt.Printf("Cep: %s\nLogradouro: %s\nBairro: %s\nCidade: %s\nEstado: %s\n",
-			response.Cep, response.Logradouro, response.Bairro, response.Cidade, response.Estado)
+		fasterApi := make(chan CEP)
+		go func() {
+			response, err := BuscaViaCep(cep)
+			if err != nil {
+				log.Fatalf("Error getting cep: %v", err)
+			}
+			msg := convertViaCEP(*response)
+			fasterApi <- msg
+		}()
 
-		responseBRAPI, err := BuscaBrasilApiCep(cep)
-		if err != nil {
-			log.Fatalf("Error getting cep: %v", err)
+		go func() {
+			responseBRAPI, err := BuscaBrasilApiCep(cep)
+			if err != nil {
+				log.Fatalf("Error getting cep: %v", err)
+			}
+			msg := convertBrasilApiCep(*responseBRAPI)
+			fasterApi <- msg
+		}()
+
+		select {
+		case msg := <-fasterApi:
+			msg.PrintAsMap()
+		case msg := <-fasterApi:
+			msg.PrintAsMap()
+		case <-time.After(1 * time.Second):
+			log.Fatalf("Timeout")
 		}
-		fmt.Println("BrasilAPI")
-		fmt.Printf("Cep: %s\nLogradouro: %s\nBairro: %s\nCidade: %s\nEstado: %s\n",
-			responseBRAPI.Cep, responseBRAPI.Logradouro, responseBRAPI.Bairro, responseBRAPI.Cidade, responseBRAPI.Estado)
 	}
 }
 
 func BuscaViaCep(cep string) (*ViaCEP, error) {
+	// time.Sleep(2 * time.Second)
 	resp, error := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
 	if error != nil {
 		return nil, error
@@ -64,6 +127,7 @@ func BuscaViaCep(cep string) (*ViaCEP, error) {
 }
 
 func BuscaBrasilApiCep(cep string) (*BrasilApiCep, error) {
+	// time.Sleep(2 * time.Second)
 	resp, error := http.Get("https://brasilapi.com.br/api/cep/v1/" + cep)
 	if error != nil {
 		return nil, error
